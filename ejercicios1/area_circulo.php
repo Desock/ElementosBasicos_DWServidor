f4dd8cb109a6f34d8b946d6cb141d792d5a2b860
Desock <salvagomezrisen@gmail.com>
2024-09-19 18:43:36 +0200

    Ejercicio 1.2 hecho

---
+++ ejercicios1/area_circulo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 2</title>
+</head>
+<body>
+    <h1>Ejercicio 2</h1>
+    <?php
+        define('PI', 3.1415);
+        $radio = 3.5;
+        $area = PI * $radio * $radio;
+
+        echo "El area del circulo es $area";  
+    ?>
+</body>
+</html>

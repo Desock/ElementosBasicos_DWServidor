d83d33ee1a6dcb5229e4239ccd5a94bb84c5d8d1
Desock <salvagomezrisen@gmail.com>
2024-09-19 18:08:08 +0200

    Ejercicio1 hecho

---
+++ ejercicios1/info_basica.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+    <h1>Ejercicio 1</h1>
+    <?php
+        $nombre = "Salva";
+        $año = "1994";
+    ?>
+    <p>Me llamo <?php echo $nombre ?> y nací en el año <?php echo $año ?></p>
+</body>
+</html>
